e4c85c581358c5f81617febb319ffaed5f62b2df
Everyday465 <110691604+Everyday465@users.noreply.github.com>
2025-05-10 15:06:49 +0800

    docker implementation

--- app.js
+++ app.js
@@ -75,6 +75,8 @@ app.post('/', (req, res) => {
     res.render('index', { data: { numbers: numbers, canUndo: history.size > 0, isSolved: solved } });
 });
 
+const PORT = process.env.PORT || 3000;
 
-
-app.listen(3001);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
